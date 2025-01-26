f3ec409221bcd87672b4ce655a63aa2ec8d3e5c0
Marcus W <rks@whq.ca>
2025-01-26 11:38:43 -0800

    Main Menu + TMP Assets

---
+++ Assets/GameAssets/Scripts/UIEvents.cs
@@ -0,0 +1,72 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIEvents : MonoBehaviour
+{
+    [SerializeField] Scene mainGame;
+
+    [SerializeField] GameObject[] mainMenuAssets;
+    [SerializeField] GameObject[] creditsAssets;
+    [SerializeField] GameObject[] optionsMenuObjects;
+
+
+    public void ExitTheGame()
+    {
+        Application.Quit();
+    }
+
+    public void StartTheGame()
+    {
+        int MMI = mainGame.buildIndex;
+        SceneManager.LoadScene(MMI);
+    }
+
+    public void EnableMainMenuAssetsFromOptions( )
+    {
+        foreach (GameObject menuObject in optionsMenuObjects)
+        {
+            menuObject.SetActive(false);
+
+        }
+        foreach (GameObject menuObject in mainMenuAssets)
+        {
+            menuObject.SetActive(true);
+        }
+    }
+    public void EnableOptionsMenuAssets()
+    {
+        foreach (GameObject menuObject in mainMenuAssets)
+        {
+            menuObject.SetActive(false);
+
+        }
+        foreach (GameObject menuObject in optionsMenuObjects)
+        {
+            menuObject.SetActive(true);
+        }
+    }
+    public void EnableCreditsMenuAssets()
+    {
+        foreach (GameObject menuObject in mainMenuAssets)
+        {
+            menuObject.SetActive(false);
+
+        }
+        foreach (GameObject menuObject in creditsAssets)
+        {
+            menuObject.SetActive(true);
+        }
+    }
+    public void EnableMainMenuAssetsfromCredits()
+    {
+        foreach (GameObject menuObject in creditsAssets)
+        {
+            menuObject.SetActive(false);
+
+        }
+        foreach (GameObject menuObject in mainMenuAssets)
+        {
+            menuObject.SetActive(true);
+        }
+    }
+}
