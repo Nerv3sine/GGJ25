331ab6f24c02d1a65604fdbd537069ddc6753ac1
Vicki <vickiszhang@gmail.com>
2025-01-25 20:48:02 -0800

    gravity

---
+++ Assets/GameAssets/Scripts/GravityPull.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class GravityPull : MonoBehaviour
+{
+    [SerializeField] Transform player;
+    Rigidbody playerBody;
+    [SerializeField] float range;
+    [SerializeField] float intensity;
+    [SerializeField] float distanceToPlayer;
+    Vector2 pullForce;
+
+    void Start()
+    {
+        playerBody = player.GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        distanceToPlayer = Vector2.Distance(player.position, transform.position);
+        if (distanceToPlayer <= range)
+        {
+            pullForce = (transform.position - player.position).normalized / distanceToPlayer * intensity;
+            playerBody.AddForce(pullForce, ForceMode.Force);
+        }
+
+    }
+}
