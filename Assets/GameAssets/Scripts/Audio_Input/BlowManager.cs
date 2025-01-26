4a9efd9ed8827cfa7ba4c70fda3b6f48f3825ccf
Nathan Helm <nathanhelm567@gmail.com>
2025-01-25 17:55:29 -0800

    added audio folder

---
+++ Assets/GameAssets/Scripts/Audio_Input/BlowManager.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+
+public class BlowManager : MonoBehaviour
+{
+    //when character blows in blowAudioManager, bubble get larger using functions from bubble manager
+
+    public static BlowManager INSTANCE;
+
+    [SerializeField]
+    private AudioSource source;
+    [SerializeField]
+    public Vector3 minScale, maxScale;
+
+    public float loudness { get; set; } = 0.0f;
+
+    [SerializeField]
+    private float loudnessSensitivity = 1.5f;
+
+    public float threshold { get; set; } = 0.05f; //USE THIS VARIABLE FOR AUDIO THRESHOLD!!!!!!!!!!!! RAAAAAAHHHH
+
+
+    public void OnEnable()
+    {
+        INSTANCE = FindFirstObjectByType<BlowManager>().GetComponent<BlowManager>();
+
+    }
+    public void Update()
+    {
+        Blow();
+    }
+   
+    private void Blow()
+    {
+        loudness = BubbleAudioManager.INSTANCE.GetLoudnessFromMicrophone();
+        if (loudness < threshold)
+        {
+            loudness = 0;
+            return;
+        }
+        Debug.Log("loudness --> " + loudness);
+
+        //bubble goes up based on intensity of the blow.
+
+
+
+        //bubble moves up
+
+        //direction move based on mouse input
+
+    }
+
+
+}
